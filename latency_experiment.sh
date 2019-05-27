692783b9ce641d90719b871074f39945ee600d21
Elkoumy <gamal.elkomy@gmail.com>
2019-05-27 11:03:27 +0300

    Adding the following: * collecting_results python file to parse redis * plotting using R (2 scripts plot_latency and plot throughput) * the folder Results contain the figures * shell script for the latency and throughput

---
+++ latency_experiment.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Define a string variable with a value
+StringVal="Welcome to linuxhint"
+approachs="aggregate scotty"
+algorithms="LL AC HLL LC FM HLLP KMV BF"
+#algorithms="DH VEB"
+#tps="1000 2000 3000"
+tps="100000 500000 1000000"
+iteration="1"
+distributions="normal"
+#distributions="right_skewed normal sorted left_skewed"
+#distributions="normal sorted"
+input_data="/root/swagExperiment/data"
+log_dir="/root/swagExperiment/log_dir/"
+out_dir="/root/swagExperiment/out_dir/"
+# Iterate the string variable using for loop
+#for val in $StringVal; do
+#    echo $val
+#done
+for i in $iteration; do
+for app in $approachs; do
+	echo $app
+	for alg in $algorithms; do
+		echo $alg
+		for t in $tps; do
+			echo $t
+			for d in $distributions; do
+				
+			/root/swagExperiment/flink-1.7.2/bin/flink run -p 32 -c ee.ut.cs.dsg.StreamCardinality.LatencyExperiment CardinalityBenchmarking-0.1-SNAPSHOT.jar $alg $app $input_data $t $d
+			java -cp ./GetResults-0.1-SNAPSHOT.jar ee.ut.cs.dsg.ExperimentResults.parsers.GetResults $alg $app $log_dir $t $d
+			
+				
+			done
+		done
+	done	
+done
+done
+
+python ./collecting_results.py $log_dir $out_dir
